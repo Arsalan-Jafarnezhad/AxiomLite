53ff18005b7c705b4f71917b8a582a4cacaf5bcf
Arsalan <arsalan.jafarnezhad.2010@gmail.com>
2026-09-16 13:06:05 +0330

    Added .dockerignore Modified base urls Moved .env.sample to root

--- configuration/configuration/urls.py
+++ configuration/configuration/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
-    path("accounts/", include("allauth.urls")),
+    # path("accounts/", include("allauth.urls")),
     path("catalog/", include("catalog.urls")),
     path("weblog/", include("weblog.urls")),
     path("questions/", include("questions.urls")),
